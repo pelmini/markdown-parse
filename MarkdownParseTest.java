17d97b49f9104ec26c77c72baab18d03079f4068
pelmini <37095187+pelmini@users.noreply.github.com>
2022-02-24 13:51:44 -0800

    Add more test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -18,5 +18,25 @@ public class MarkdownParseTest {
         List<String> expect = List.of("https://something.com", "some-page.html");
         assertEquals(MarkdownParse.getLinks(contents), expect);
     }
-    
+
+    @Test
+    public void testFile10() throws IOException {
+        String contents= Files.readString(Path.of("./test-file10.md"));
+        List<String> expect = List.of("`google.com");
+        assertEquals(MarkdownParse.getLinks(contents), expect);
+    }    
+
+    @Test
+    public void testFile11() throws IOException {
+        String contents= Files.readString(Path.of("./test-file11.md"));
+        List<String> expect = List.of("a.com", "a.com(())", "example.com");
+        assertEquals(MarkdownParse.getLinks(contents), expect);
+    }  
+
+    @Test
+    public void testFile12() throws IOException {
+        String contents= Files.readString(Path.of("./test-file12.md"));
+        List<String> expect = List.of("https://ucsd-cse15l-w22.github.io/");
+        assertEquals(MarkdownParse.getLinks(contents), expect);
+    }  
 }
